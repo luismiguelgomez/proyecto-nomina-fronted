1d01db3fcf3bdbf63bd2bd02528d2fd835705b9c
cfuen <SapoPerroGitHub>
2021-11-26 16:49:23 -0500

    Ahora sí, todo en órden. Todas las dependencias están bacanas, procederé a elaborar el dashboard

--- src/app/huh/app.module.ts
+++ src/app/huh/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '../viu/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http'
